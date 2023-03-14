308a35f731a4b79ea0a1c1f1887eee6982925d64
Cooltor <romaindbus86@gmail.com>
2023-03-14 11:14:57 +0100

    git ignore maj

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NewfactForm from "./newFactForm";
 import FactList from "./factList";
 
 function App() {
+  const appTitle = "Today I Learned";
   return (
     <>
       {/*HEADER$*/}
@@ -15,7 +16,7 @@ function App() {
             width="68"
             alt="Today I Learned Logo"
           />
-          <h1>Today I Learned</h1>
+          <h1>{appTitle}</h1>
         </div>
 
         <button className="btn btn-large btn-open">Share a fact</button>
